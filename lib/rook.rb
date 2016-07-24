ccb38fe192ecea5f82df3f0d01810e0f74c39969
DV <dvislearning@gmail.com>
2016-07-24 01:51:39 -0700

    Modify rook methods to resemble bishop

--- lib/rook.rb
+++ lib/rook.rb
@@ -36,11 +36,74 @@ class Rook < Piece
 		 end
 		 path_container.delete(curr)
 		 path_container
-	end		
+	end	
+
+	def move_right(beginning, destination)
+		beg = beginning
+		path = []
+		while beg != destination
+			return false if beg[1] > 7
+			path << beg
+			beg = [(beg[0]), (beg[1] + 1)]
+		end
+		path << destination
+		path.delete(beginning)
+		path
+	end
+
+	def move_left(beginning, destination)
+		beg = beginning
+		path = []
+		while beg != destination
+			return false if beg[1] < 0
+			path << beg
+			beg = [(beg[0]), (beg[1] - 1)]
+		end
+		path << destination
+		path.delete(beginning)
+		path
+	end
+
+	def move_up(beginning, destination)
+		beg = beginning
+		path = []
+		while beg != destination
+			return false if beg[0] > 7
+			path << beg
+			beg = [(beg[0] + 1), (beg[1])]
+		end
+		path << destination
+		path.delete(beginning)
+		path
+	end
+
+	def move_down(beginning, destination)
+		beg = beginning
+		path = []
+		while beg != destination
+			return false if beg[0] < 0
+			path << beg
+			beg = [(beg[0] -  1), (beg[1])]
+		end
+		path << destination
+		path.delete(beginning)
+		path
+	end	
+
+	def move_path(beginning, destination)
+		paths = Array.new
+		paths << move_up(beginning, destination)
+		paths << move_down(beginning, destination)
+		paths << move_left(beginning, destination)
+		paths << move_right(beginning, destination)
+		result = paths.find { |path| path != false }
+		result == nil ? false : result 
+	end
+
 end
 
-#  a = Rook.new("black")
-# # puts a.valid_path?("A4", "D4").inspect
+   a = Rook.new("black")
+# puts a.move_up([3,5], [9,5]).inspect
 # puts a.move_path("E4", "B4").inspect
 # puts a.move_path("B6", "B3").inspect
 # puts a.move_path("B8", "B4").inspect
--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -27,36 +27,88 @@ describe 'Rook' do
 		end				
 	end
 
-	describe '#valid_path?' do
-		it 'returns true when moving along a file' do
-			@debug_board.place_piece("A2", :brook)
-			expect(@rook.valid_path?("A2", "D2")).to eq(true)
+	describe '#move_right' do
+		it 'returns path when rook makes valid right move' do
+			expect(@rook.move_right([6,4], [6,7])).to eq([[6,5], [6,6], [6,7]])
 		end
 
-		it 'returns true when moving along a rank' do
-			@debug_board.place_piece("A2", :brook)
-			expect(@rook.valid_path?("A2", "A8")).to eq(true)
-		end
-
-		it 'returns false when not moving along rank or file' do
-			@debug_board.place_piece("A2", :brook)
-			expect(@rook.valid_path?("A2", "B3")).to eq(false)
-		end				
+		it 'returns false when rook makes invalid right move' do
+			expect(@rook.move_right([6,4], [6,2])).to eq(false)
+		end		
 	end
 
-	describe '#move_path' do
-		it 'returns array with path rook has to take vertically' do
-			@debug_board.place_piece("B8", :brook)
-			expect(@rook.move_path("B8", "B4")).to eql([[3, 1], [4, 1], [5, 1], [6, 1]])
-			expect(@rook.move_path("B4", "B8")).to eql([[4, 1], [5, 1], [6, 1], [7, 1]])
+	describe '#move_left' do
+		it 'returns path when rook makes valid left move' do
+			expect(@rook.move_left([2,3], [2,2])).to eq([[2,2]])
 		end
 
-		it 'returns array with path rook has to take horizontally' do
-			@debug_board.place_piece("C4", :brook)
-			expect(@rook.move_path("C4", "F4")).to eql([[3, 3], [3, 4], [3, 5]])
-			expect(@rook.move_path("F4", "C4")).to eql([[3, 2], [3, 3], [3, 4]])
+		it 'returns false when rook makes invalid left move' do
+			expect(@rook.move_left([6,4], [5,2])).to eq(false)
+		end		
+	end
+
+	describe '#move_up' do
+		it 'returns path when rook makes valid up move' do
+			expect(@rook.move_up([1,3], [5,3])).to eq([[2,3], [3,3], [4,3], [5,3]])
 		end
-	end			
+
+		it 'returns false when rook makes invalid up move' do
+			expect(@rook.move_up([3,5], [9,5])).to eq(false)
+		end		
+	end
+
+	describe '#move_down' do
+		it 'returns path when rook makes valid down move' do
+			expect(@rook.move_down([3,2], [0,2])).to eq([[2,2], [1,2], [0,2]])
+		end
+
+		it 'returns false when rook makes invalid down move' do
+			expect(@rook.move_down([3,2], [-2,2])).to eq(false)
+		end		
+	end	
+
+	describe '#move_path' do
+		it 'returns path when valid rook move is given as argument' do
+			expect(@rook.move_path([3,1], [3,6])).to eq([[3,2], [3,3], [3,4], [3,5], [3,6]])
+		end
+
+		it 'returns false when rook makes invalid move' do
+			expect(@rook.move_path([2,0], [3,5])).to eq(false)
+			expect(@rook.move_path([0,0], [6,6])).to eq(false)
+			expect(@rook.move_path([7,1], [5,2])).to eq(false)
+			expect(@rook.move_path([6,4], [3,7])).to eq(false)
+		end
+	end	
+	# describe '#valid_path?' do
+	# 	it 'returns true when moving along a file' do
+	# 		@debug_board.place_piece("A2", :brook)
+	# 		expect(@rook.valid_path?("A2", "D2")).to eq(true)
+	# 	end
+
+	# 	it 'returns true when moving along a rank' do
+	# 		@debug_board.place_piece("A2", :brook)
+	# 		expect(@rook.valid_path?("A2", "A8")).to eq(true)
+	# 	end
+
+	# 	it 'returns false when not moving along rank or file' do
+	# 		@debug_board.place_piece("A2", :brook)
+	# 		expect(@rook.valid_path?("A2", "B3")).to eq(false)
+	# 	end				
+	# end
+
+	# describe '#move_path' do
+	# 	it 'returns array with path rook has to take vertically' do
+	# 		@debug_board.place_piece("B8", :brook)
+	# 		expect(@rook.move_path("B8", "B4")).to eql([[3, 1], [4, 1], [5, 1], [6, 1]])
+	# 		expect(@rook.move_path("B4", "B8")).to eql([[4, 1], [5, 1], [6, 1], [7, 1]])
+	# 	end
+
+	# 	it 'returns array with path rook has to take horizontally' do
+	# 		@debug_board.place_piece("C4", :brook)
+	# 		expect(@rook.move_path("C4", "F4")).to eql([[3, 3], [3, 4], [3, 5]])
+	# 		expect(@rook.move_path("F4", "C4")).to eql([[3, 2], [3, 3], [3, 4]])
+	# 	end
+	# end			
 
 
 	# 	it 'returns false when rook\'s path is obstructed' do
