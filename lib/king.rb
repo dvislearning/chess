044193de3174bb62ccbbf58178c95fe35d30fea4
DV <dvislearning@gmail.com>
2016-08-01 18:01:06 -0700

    write king move methods

--- lib/king.rb
+++ lib/king.rb
@@ -11,6 +11,20 @@ class King
 	def assign_symbol
 		@symbol = @color == "white" ? "\u{2654} " : "\u{265A} "
 	end 
+
+	def move_path(beginning, destination)
+		beg = beginning
+		king_moves = [[1,-1], [1,0], [1,1], [0,-1], [0,1], [-1,-1], [-1,0], [-1,1]]
+		possible_moves = Array.new
+		king_moves.map { |move| possible_moves << [(beg[0]+move[0]), (beg[1]+move[1])] }
+		move = possible_moves.select { |poss| poss == destination}
+		move.empty? ? false : move
+	end		
 end
 
+#How do I check if the king is being checked?  
+#Have him do the lehal move of every piece.
+#If he encounters a piece of the opposite color, heis in check.
 
+a = King.new("black")
+puts a.move_path([3,5], [5,5]).inspect
--- spec/king_spec.rb
+++ spec/king_spec.rb
@@ -24,4 +24,24 @@ describe 'King' do
 			expect(@white_king.symbol).to eq("\u{2654} ")
 		end				
 	end
+
+	describe '#move_path' do
+		it 'returns path when valid king move is given as argument' do
+			expect(@king.move_path([3,3], [4,2])).to eq([[4,2]])
+			expect(@king.move_path([3,3], [4,3])).to eq([[4,3]])
+			expect(@king.move_path([3,3], [4,4])).to eq([[4,4]])
+			expect(@king.move_path([3,3], [3,2])).to eq([[3,2]])
+			expect(@king.move_path([3,3], [3,4])).to eq([[3,4]])
+			expect(@king.move_path([3,3], [2,2])).to eq([[2,2]])
+			expect(@king.move_path([3,3], [2,3])).to eq([[2,3]])
+			expect(@king.move_path([3,3], [2,4])).to eq([[2,4]])
+		end
+
+		it 'returns false when king makes invalid move' do
+			expect(@king.move_path([3,3], [1,1])).to eq(false)
+			expect(@king.move_path([3,3], [5,4])).to eq(false)
+			expect(@king.move_path([3,3], [1,3])).to eq(false)
+			expect(@king.move_path([3,3], [3,1])).to eq(false)
+		end
+	end		
 end
